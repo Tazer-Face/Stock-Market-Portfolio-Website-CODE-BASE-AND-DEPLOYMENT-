7c6906669877b8981c42206003e04fa465d0f69f
Tazer-Face <160270570+Tazer-Face@users.noreply.github.com>
2025-11-17 16:59:41 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // apply to all routes
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              connect-src 'self' https://stock-market-portfolio-website-code-base.onrender.com wss://stock-market-portfolio-website-code-base.onrender.com;
+              img-src 'self' data:;
+              script-src 'self';
+              style-src 'self' 'unsafe-inline';
+            `.replace(/\n/g, " "),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
