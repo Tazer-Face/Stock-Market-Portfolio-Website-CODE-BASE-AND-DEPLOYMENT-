a9c062faaca316beedc1cdae7d9a8ca73796dde5
Tazer-Face <160270570+Tazer-Face@users.noreply.github.com>
2025-11-17 15:04:41 +0530

    Add files via upload

---
+++ backend/src/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const stockRoutes = require("./routes/stockRoutes");
+const {connectDB,disconnectDB} = require("./config/databases");
+const stockData = require("./Stock data/stockData");
+const cors = require('cors');
+require('dotenv').config();
+
+
+connectDB();
+
+const port = process.env.PORT;
+
+const server = app.listen(port ,() => {
+  console.log("App + WebSocket running on port 4005");
+});
+
+
+stockData(server);
+
+app.use(cors({
+  origin: process.env.CLIENT_ORIGIN,
+  credentials: true
+}));
+
+app.use(express.json());
+
+app.use('/api/stocks',stockRoutes);
+
+process.on('SIGINT',() => disconnectDB('SIGINT'));
+process.on('SIGTERM',() => disconnectDB('SIGTERM'));
+
+app.listen(3000,()=>{console.log("server running on port 3000")});
