c76d781c475432011bec0e47f6efaef9e2e7a33d
Tazer-Face <160270570+Tazer-Face@users.noreply.github.com>
2026-03-13 03:58:33 +0530

    Update stockData.js
    Changed architecture

--- backend/src/Stock data/stockData.js
+++ backend/src/Stock data/stockData.js
@@ -2,7 +2,7 @@ const { WebSocketServer } = require("ws");
 const yahooFinance = require("yahoo-finance2").default;
 const { GoogleSpreadsheet } = require("google-spreadsheet");
 const { JWT } = require("google-auth-library");
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = function (server) {
   const tickers = [
@@ -34,16 +34,29 @@ module.exports = function (server) {
     "SBILIFE.NS",
   ];
 
- 
+  const yf = new yahooFinance();
+
   const wss = new WebSocketServer({ server });
+
+  const tickerAlias = {
+    BLS: "BLSE",
+  };
+
+  const clients = new Set();
+
+  let intervalId = null;
+
+  const normalize = (ticker) => {
+    if (!ticker) return "";
+    const t = ticker.split(".")[0].trim().toUpperCase();
+    return tickerAlias[t] || t;
+  };
 
   let cachedFinalData = [];
   let cachedErrors = [];
-  let lastFetchTime = 0;
-  const CACHE_DURATION = 15000;
 
   const GOOGLE_CACHE_DURATION = 5 * 60 * 1000;
-  let googleCache = [];
+  let googleCache = new Map();
   let lastGoogleFetch = 0;
 
   const auth = new JWT({
@@ -57,129 +70,175 @@ module.exports = function (server) {
   async function loadGoogleSheetData() {
     const now = Date.now();
 
-    if (now - lastGoogleFetch < GOOGLE_CACHE_DURATION && googleCache.length > 0) {
+    if (now - lastGoogleFetch < GOOGLE_CACHE_DURATION && googleCache.size > 0) {
       return googleCache;
     }
 
     try {
       await doc.loadInfo();
       const sheet = doc.sheetsByIndex[0];
-      await sheet.loadCells();
-      const rowCount = sheet.rowCount;
-      const data = []
-  
-      for (let i = 1; i < rowCount; i++) {
-        const company = sheet.getCell(i, 0).value;
-        const sector = sheet.getCell(i, 1).value;
-        const ticker = sheet.getCell(i, 2).value;
-        const pe = sheet.getCell(i, 3).value;
-        const eps = sheet.getCell(i, 4).value;
-  
-        if (!company) continue;
-    
-        data.push({
-            company,
-            sector,
-            ticker,
-            pe,
-            eps,
-          });
-      }
+      //await sheet.loadCells();
+      // const rowCount = sheet.rowCount;
+      // const data = []
 
-      googleCache = data;
+      // for (let i = 1; i < rowCount; i++) {
+      //   const company = sheet.getCell(i, 0).value;
+      //   const sector = sheet.getCell(i, 1).value;
+      //   const ticker = sheet.getCell(i, 2).value;
+      //   const pe = sheet.getCell(i, 3).value;
+      //   const eps = sheet.getCell(i, 4).value;
+
+      //   if (!company) continue;
+
+      //   data.push({
+      //       company,
+      //       sector,
+      //       ticker,
+      //       pe,
+      //       eps,
+      //     });
+      // }
+
+      let sheetMap = new Map();
+
+      const rows = await sheet.getRows();
+
+      rows.forEach((row) => {
+        sheetMap.set(normalize(row.ticker), {
+          company: row.company,
+          sector: row.sector,
+          ticker: row.ticker,
+          pe: row.pe,
+          eps: row.eps,
+        });
+      });
+
+      // const data = rows.map((row) => ({
+      //   company: row.company,
+      //   sector: row.sector,
+      //   ticker: row.ticker,
+      //   pe: row.pe,
+      //   eps: row.eps,
+      // }));
+
+      googleCache = sheetMap;
       lastGoogleFetch = now;
 
-      return data;
+      return sheetMap;
     } catch (err) {
       console.error("Google Sheets Error:", err);
-      return [];
+      return new Map();
     }
   }
-  
+
   wss.on("connection", (ws) => {
     console.log("WS client connected");
+    clients.add(ws);
 
-    const interval = async () => {
-      try {
+    if (clients.size === 1) {
+      intervalId = setInterval(fetchAndBroadcast, 15000);
+    }
 
-        const now = Date.now();
+    if (cachedFinalData.length > 0) {
+      ws.send(
+        JSON.stringify({
+          data: cachedFinalData,
+          error: cachedErrors,
+        }),
+      );
+    }
 
-        if (now - lastFetchTime < CACHE_DURATION && cachedFinalData.length > 0) {
-          ws.send(JSON.stringify({
-            data: cachedFinalData,
-            errors: cachedErrors
-          }));
-          return;
-        }
-
-        lastFetchTime = now;
-        let errors = []
-        let data = {};
-        let googleJson = [];
-
-        const yf = new yahooFinance();
-        try{
-          data = await yf.quote(tickers);
-        }
-        catch(err){
-          console.error("Error fetching Yahoo data:", err);
-          errors.push({
-            source : "Yahoo Finance",
-            message : err.message
-          })
-          data = {};
-        }
-
-        try {
-           googleJson = await loadGoogleSheetData();
-        } catch (err) {
-           console.error("Error fetching Google data:", err);
-           errors.push({
-            source : "Google Sheets",
-            message : err.message
-          })
-          googleJson = [];
-        }
-        
-        const arr = Object.values(data).map((res) => ({
-            ticker: res.symbol,
-            price: res.regularMarketPrice,
-            name: res.shortName,
-            previousClose : res.regularMarketPreviousClose,
-        }));
-
-        const tickerAlias = {
-          "BLS" : "BLSE"
-        }
-        const normalize = (ticker) =>{
-        if (!ticker) return "";
-        const t = ticker.split(".")[0].trim().toUpperCase();
-        return tickerAlias[t] || t;
-        }
-
-        const finalData =arr.map(item => {
-          const sheetItem = googleJson.find(
-            (sheetRes) => normalize(sheetRes.ticker) === normalize(item.ticker))
-            return sheetItem
-                    ? { ...item, pe: sheetItem.pe , eps: sheetItem.eps}
-                    : item;
-        });
-
-        cachedFinalData = finalData;
-        cachedErrors = errors;
-
-        ws.send( JSON.stringify({
-          data: finalData,
-          error: errors
-        }));
-      } catch (err) {
-        console.error("Error fetching Yahoo and Google data:", err);
+    ws.on("close", () => {
+      clients.delete(ws);
+      if (clients.size === 0) {
+        clearInterval(intervalId);
+        intervalId = null;
       }
-    };
-
-    const intervalId = setInterval(interval, 15000);
-
-    ws.on("close", () => clearInterval(intervalId));
+    });
   });
 
+  const fetchAndBroadcast = async () => {
+    try {
+      let errors = [];
+      let data = {};
+      let googleMap;
+
+      try {
+        data = await yf.quote(tickers);
+      } catch (err) {
+        console.error("Error fetching Yahoo data:", err);
+        errors.push({
+          source: "Yahoo Finance",
+          message: err.message,
+        });
+        data = {};
+      }
+
+      try {
+        googleMap = await loadGoogleSheetData();
+      } catch (err) {
+        console.error("Error fetching Google data:", err);
+        errors.push({
+          source: "Google Sheets",
+          message: err.message,
+        });
+        googleMap = new Map();
+      }
+
+      // const arr = Object.values(data || {}).map((res) => ({
+      //     ticker: res.symbol,
+      //     price: res.regularMarketPrice,
+      //     name: res.shortName,
+      //     previousClose : res.regularMarketPreviousClose,
+      // }));
+
+      let rawData = new Map();
+
+      Object.values(data || {}).forEach((row) => {
+        rawData.set(normalize(row.symbol), row);
+      });
+
+      const arr = tickers.map((value) => {
+        let res = rawData.get(normalize(value));
+        if (!res) {
+          return {
+            ticker: value,
+            price: null,
+            name: value,
+            previousClose: null,
+          };
+        }
+
+        return {
+          ticker: res.symbol,
+          price: res.regularMarketPrice || null,
+          name: res.shortName,
+          previousClose: res.regularMarketPreviousClose || null,
+        };
+      });
+
+      const finalData = arr.map((item) => {
+        const sheetItem = googleMap.get(normalize(item.ticker));
+        return sheetItem
+          ? { ...item, pe: sheetItem.pe, eps: sheetItem.eps }
+          : item;
+      });
+
+      cachedFinalData = finalData;
+      cachedErrors = errors;
+
+      clients.forEach((client) => {
+        if (client.readyState === 1) {
+          client.send(
+            JSON.stringify({
+              data: finalData,
+              errors,
+            }),
+          );
+        }
+      });
+    } catch (err) {
+      console.error("Error fetching Yahoo and Google data:", err);
+    }
+  };
 };
